e86e77a5249a253cc906703a32d61e0fe96e485c
Marvin <KillingSausage@gmail.com>
2024-01-05 17:57:24 +0100

    Revert "main"
    This reverts commit 42e9117d6a930ded63dc25fca0d5a7580ebc712c.

--- manager/pieces/bishop.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Marvin Becker on 05.01.24.
-//
-
-#include "bishop.h"
-
-bool canBishopMove(int x, int y, int move_x, int move_y) {
-  return std::abs(y - move_y) == std::abs(x - move_x);
-}
