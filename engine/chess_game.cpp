c87595aba4747dbd80cc618cc3a87a8d2ffe4f16
Marvin <KillingSausage@gmail.com>
2024-03-16 18:42:51 +0100

    Print board state after each move
    The update in the code ensures that the current state of the board is printed after each move. This includes both the moves made by the human player and the bot. This provides a clearer understanding of the ongoing game state and aids debugging in case of any issues.

--- engine/chess_game.cpp
+++ engine/chess_game.cpp
@@ -48,16 +48,18 @@ void ChessGame::start() {
     }
 
     if (board->movePiece(figure, col, row, move_col, move_row, capture, promotion_figure)) {
+      board->printCurrentBoard();
       if (board->isCheckMate(board->player == WHITE)) {
         std::cout << "CHECK MATE!" << std::endl;
         return;
       }
+
       board->makeMove(bot->searchBestNextMove(*board, 5));
       if (board->isCheckMate(board->player == WHITE)) {
+        board->printCurrentBoard();
         std::cout << "CHECK MATE!" << std::endl;
         return;
       }
-      board->printCurrentBoard();
     }
   }
 }
