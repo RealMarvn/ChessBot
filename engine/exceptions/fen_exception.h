db3c4ec7d15733bbc810c89b69a99014797435b8
Marvin <KillingSausage@gmail.com>
2024-03-18 17:36:54 +0100

    Added a Fen exception (cannot do anything rn)

---
+++ engine/exceptions/fen_exception.h
@@ -0,0 +1,17 @@
+//
+// Created by Marvin Becker on 18.03.24.
+//
+
+#pragma once
+
+#include <exception>
+#include <string>
+#include <utility>
+
+class InvalidFENException : public std::exception {
+public:
+    explicit InvalidFENException() = default;
+    [[nodiscard]] const char* what() const noexcept override {
+        return "FEN is invalid!!!";
+    }
+};
